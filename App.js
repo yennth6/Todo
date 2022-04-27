3a9f2a930b97a0ae51747d37663058eef282e2aa
Nguyễn Thị Hải Yến <yennth6@rikkeisoft.com>
2022-04-27 12:05:18 +0700

    finish

--- App.js
+++ App.js
@@ -1,11 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+
+import Task from './components/Task'
+import Form from './components/Form';
 
 export default function App() {
+  const [todoList, setTodoList] = useState([])
+
+  const onTodoAdded = (todo) => {
+    console.log("onTodoAdded", todo)
+    setTodoList([...todoList, {id: todoList.length + 1, title: todo}])
+  }
+
+  // useEffect(()=> {
+  //   console.log(todoList)
+  // }, [todoList])
+  const alert = (index) => {
+    Alert.alert(
+      "Alert",
+      "Do you really want to delete this task?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { 
+          text: "OK", 
+          onPress: () => {
+            console.log("Alert", index)
+
+            console.log("OK Pressed")
+            let list = [...todoList]
+            list.splice(index-1, 1)
+            setTodoList(list)
+          } 
+        }
+      ]
+    );
+  }
+  const renderItem =({item}) => {
+    return (
+      <Task 
+        alert={alert}
+        title={item.title} 
+        index={item.id}/>
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.body}>
+        <View style={styles.title}>
+          <Text style={styles.textTitle}>Todo List</Text>
+        </View>
+        <FlatList style={styles.listTodo}
+          data={todoList}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}>
+        
+        </FlatList>
+        <View style={styles.formAdd}>
+          <Form onTodoAdded={onTodoAdded}></Form>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +70,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f0f9ff'
   },
+  body: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginTop: 40
+  },
+  title: {
+    // marginTop: 20
+  },
+  textTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#52b5f7'
+  },
+  listTodo: {
+    marginTop: 20
+  },
+  formAdd: {
+    marginVertical: 10
+  }
 });
---
+++ components/Task/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import styles from './style';
+const Task = (props) => {
+    const {index} = props
+    const indexText = index < 10 ? `0${index}` : index
+    const backGroundType = index % 2 ? styles.odd : styles.even
+    return (
+        <TouchableOpacity
+          onPress={() => props.alert(index)}>
+            <View style={styles.item}>
+              <View style={[styles.index, backGroundType]}>
+                <Text style={styles.textIndex}>{indexText}</Text>
+              </View>
+              <Text style={styles.todoDetail}>{props.title}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default Task
